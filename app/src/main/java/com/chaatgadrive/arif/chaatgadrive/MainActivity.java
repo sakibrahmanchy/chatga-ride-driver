34e38efbad6f7b2c4ce70572d4ac18a11d8c4e87
ariful islam <arifulcuet52@gmail.com>
2017-12-09 03:40:39 +0600

    module.xml file change

--- app/src/main/java/com/chaatgadrive/arif/chaatgadrive/MainActivity.java
+++ app/src/main/java/com/chaatgadrive/arif/chaatgadrive/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.chaatgadrive.arif.chaatgadrive.chaatgamap.Mapfragment;
+import com.chaatgadrive.arif.chaatgadrive.dashboard.DashboardFragment;
+
 
 import ContactWithFirebase.Main;
 import __Firebase.FirebaseUtility.FirebaseConstant;
@@ -18,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
     private Mapfragment mapfragment = new Mapfragment();
+    private DashboardFragment dashboardFragment = new DashboardFragment();
+   private   FragmentManager manager = getSupportFragmentManager();
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -26,14 +30,26 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
 
-                    FragmentManager manager = getSupportFragmentManager();
-                    manager.beginTransaction().replace(R.id.mapView,mapfragment,mapfragment.getTag()).commit();
+                    manager.beginTransaction().replace(R.id.content,mapfragment,mapfragment.getTag()).commit();
                     return true;
                 case R.id.navigation_dashboard:
                  //   mTextMessage.setText(R.string.title_dashboard);
+
+                    manager.beginTransaction().replace(R.id.content,dashboardFragment,dashboardFragment.getTag()).commit();
+
+
                     return true;
                 case R.id.navigation_notifications:
-                   // mTextMessage.setText(R.string.title_notifications);
+
+                    manager.beginTransaction().replace(R.id.content,dashboardFragment,dashboardFragment.getTag()).commit();
+                    return true;
+                case R.id.navigation_earning:
+
+                    manager.beginTransaction().replace(R.id.content,dashboardFragment,dashboardFragment.getTag()).commit();
+                    return true;
+                case R.id.navigation_profile:
+
+                    manager.beginTransaction().replace(R.id.content,dashboardFragment,dashboardFragment.getTag()).commit();
                     return true;
             }
             return false;
@@ -50,14 +66,13 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        manager.beginTransaction().replace(R.id.content,mapfragment,mapfragment.getTag()).commit();
         Main main = new Main();
-        //main.CreateNewRiderFirebase();
-        //main.CreateNewHistoryModelFirebase();
-        main.GetCurrentRider(1104006); /*Save this ID SQ_LITE or somewhere else*/
-        //main.GetRecentHistory(10101010); /*Save this ID SQ_LITE or somewhere else*/
-        //main.SetRiderBusyOrFre(FirebaseWrapper.getInstance().getRiderModelInstance(), FirebaseConstant.SET_RIDER_BUSY);
-        //main.SetRiderOnRideOrFree(FirebaseWrapper.getInstance().getRiderModelInstance(), FirebaseConstant.SET_RIDER_ON_RIDE);
-        //SetRiderOnLineOrOffLine(FirebaseWrapper.getInstance().getRiderModelInstance(), FirebaseConstant.SET_RIDER_OFF_ONLINE);
+        main.CreateNewRiderFirebase();
+    }
+
+
 
     }
-}
+
+
