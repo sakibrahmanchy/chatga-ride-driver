7908055a2921611fb6a4a5422112ae116861b7ab
ariful islam <arifulcuet52@gmail.com>
2017-12-10 04:17:52 +0600

    module.xml file change

--- app/src/main/java/com/chaatgadrive/arif/chaatgadrive/dashboard/DashboardFragment.java
+++ app/src/main/java/com/chaatgadrive/arif/chaatgadrive/dashboard/DashboardFragment.java
@@ -1,30 +1,105 @@
 package com.chaatgadrive.arif.chaatgadrive.dashboard;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 import com.chaatgadrive.arif.chaatgadrive.R;
-import com.chaatgadrive.arif.chaatgadrive.chaatgamap.ConstentUtilityModel;
-import com.chaatgadrive.arif.chaatgadrive.chaatgamap.GetCurrentLocation;
-import com.google.android.gms.maps.SupportMapFragment;
 
 /**
  * Created by Arif on 12/8/2017.
  */
 
 public class DashboardFragment extends Fragment {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View mapview = inflater.inflate(R.layout.dashboard_fragment, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dashboard_fragment, container, false);
 
+        viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+        tabLayout = (TabLayout) view.findViewById(R.id.tabs);
 
+        return view;
+    }
 
-        return mapview;
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        FragmentManager cfManager=getChildFragmentManager();
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(cfManager);
+
+        viewPagerAdapter.addFragment(new DailyTopEarningFragment(), "First");
+        viewPagerAdapter.addFragment(new DailyTopTripsFragment(), "Second");
+        viewPager.setAdapter(viewPagerAdapter);
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        List<Fragment> fragmentList = new ArrayList<>();
+        List<String> fragmentTitles = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitles.get(position);
+        }
+
+        public void addFragment(Fragment fragment, String name) {
+            fragmentList.add(fragment);
+            fragmentTitles.add(name);
+        }
     }
 }
