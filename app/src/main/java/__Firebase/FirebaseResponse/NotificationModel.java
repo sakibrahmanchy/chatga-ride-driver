03c90d46618954e4660754ff3b6f96390de43e10
Jobayer sheikh <jobayersheikhbd@gmail.com>
2018-01-01 01:33:42 +0600

    Notification add to main activity

--- app/src/main/java/__Firebase/FirebaseResponse/NotificationModel.java
+++ app/src/main/java/__Firebase/FirebaseResponse/NotificationModel.java
@@ -6,16 +6,21 @@ package __Firebase.FirebaseResponse;
 
 public class NotificationModel {
 
-    public String title, body;
+    public String title, body, clientName, clientPhone, sourceName, destinationName;
     public long clientId;
     public double sourceLatitude, sourceLongitude, destinationLatitude, destinationLongitude;
 
-    public NotificationModel(){}
+    public NotificationModel() {
+    }
 
-    public NotificationModel(String title, String body, long clientId, double sourceLatitude, double sourceLongitude, double destinationLatitude, double destinationLongitude){
+    public NotificationModel(String title, String body, String clientName, String clientPhone, String sourceName, String destinationName, long clientId, double sourceLatitude, double sourceLongitude, double destinationLatitude, double destinationLongitude) {
         this.title = title;
         this.body = body;
         this.clientId = clientId;
+        this.clientName = clientName;
+        this.clientPhone = clientPhone;
+        this.sourceName = sourceName;
+        this.destinationName = destinationName;
         this.sourceLatitude = sourceLatitude;
         this.sourceLongitude = sourceLongitude;
         this.destinationLatitude = destinationLatitude;
