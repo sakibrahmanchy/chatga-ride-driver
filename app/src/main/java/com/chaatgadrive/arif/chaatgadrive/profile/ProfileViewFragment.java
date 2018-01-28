cc3a59cb66ecd20b59973d67420e41f611e35bc4
ariful islam <arifulcuet52@gmail.com>
2018-01-29 01:28:57 +0600

    Merge branch 'master' of https://gitlab.com/arifulcuet52/devRider
    # Conflicts:
    #	.idea/modules.xml

--- app/src/main/java/com/chaatgadrive/arif/chaatgadrive/profile/ProfileViewFragment.java
+++ app/src/main/java/com/chaatgadrive/arif/chaatgadrive/profile/ProfileViewFragment.java
@@ -1,21 +1,28 @@
 package com.chaatgadrive.arif.chaatgadrive.profile;
 
+import android.icu.util.ValueIterator;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.chaatgadrive.arif.chaatgadrive.R;
+import com.chaatgadrive.arif.chaatgadrive.SharedPreferences.UserInformation;
 
 /**
  * Created by Arif on 12/23/2017.
  */
 public class ProfileViewFragment extends Fragment{
 
-
+    private TextView user_Name;
+    private TextView phone_number;
+    private TextView mail;
+    private UserInformation userInformation;
     public ProfileViewFragment() {
     }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,16 @@ public class ProfileViewFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.profile_view, container, false);
+        View view = inflater.inflate(R.layout.profile_view, container, false);
+        user_Name = view.findViewById(R.id.name);
+        phone_number =view.findViewById(R.id.field_phone_number);
+        mail = view.findViewById(R.id.email);
+        userInformation = new UserInformation(getContext());
+
+        user_Name.setText(userInformation.getuserInformation().firstName);
+        phone_number.setText(userInformation.getRiderPhoneNumber());
+
+
+        return view;
     }
 }
