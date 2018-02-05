b0115ea58859439e9f9b640ba49c3b39f90020c2
ariful islam <arifulcuet52@gmail.com>
2018-02-06 00:26:08 +0600

    Merge branch 'master' of https://gitlab.com/arifulcuet52/devRider
    # Conflicts:
    #	app/src/main/java/com/chaatgadrive/arif/chaatgadrive/MainActivity.java

---
+++ app/src/main/java/com/chaatgadrive/arif/chaatgadrive/FirstAppLoadingActivity/FirstAppLoadingActivity.java
@@ -0,0 +1,15 @@
+package com.chaatgadrive.arif.chaatgadrive.FirstAppLoadingActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.chaatgadrive.arif.chaatgadrive.R;
+
+public class FirstAppLoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_app_loading);
+    }
+}
