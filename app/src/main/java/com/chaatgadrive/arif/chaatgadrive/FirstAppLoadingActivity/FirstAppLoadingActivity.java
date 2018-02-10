2fe63d13e37caa5f45f974243ea2665e6d7aa419
ariful islam <arifulcuet52@gmail.com>
2018-02-11 02:09:51 +0600

    Merge branch 'master' of https://gitlab.com/arifulcuet52/devRider
    # Conflicts:
    #	.idea/modules.xml

--- app/src/main/java/com/chaatgadrive/arif/chaatgadrive/FirstAppLoadingActivity/FirstAppLoadingActivity.java
+++ app/src/main/java/com/chaatgadrive/arif/chaatgadrive/FirstAppLoadingActivity/FirstAppLoadingActivity.java
@@ -42,6 +42,7 @@ public class FirstAppLoadingActivity extends AppCompatActivity {
         else{
             Intent intent = new Intent(FirstAppLoadingActivity.this, MainActivity.class);
             startActivity(intent);
+            finish();
         }
 
     }
