deb2d1650a53d3d3cab97b51a1efb7c6375d9742
Sakib Rahman <sakib.cse11.cuet@gmail.com>
2018-02-16 03:56:16 +0600

    Merge branch 'master' of https://gitlab.com/arifulcuet52/devRider
    # Conflicts:
    #	.idea/modules.xml

---
+++ app/src/main/java/com/chaatgadrive/arif/chaatgadrive/Adapters/History/DocumentsActivity.java
@@ -0,0 +1,15 @@
+package com.chaatgadrive.arif.chaatgadrive.Adapters.History;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.chaatgadrive.arif.chaatgadrive.R;
+
+public class DocumentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_documents);
+    }
+}
